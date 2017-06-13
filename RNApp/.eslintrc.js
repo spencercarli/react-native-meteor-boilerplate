5d1e4131ee8c22d86b046e197e2b307d8425fe63
Spencer Carli <spencercarli@gmail.com>
2017-06-13 09:02:56 -0500

    Upgrade other deps

--- RNApp/.eslintrc.js
+++ RNApp/.eslintrc.js
@@ -19,9 +19,6 @@ module.exports = {
   },
   "parserOptions": {
     "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true
-    }
+    "sourceType": "module"
   },
 };
